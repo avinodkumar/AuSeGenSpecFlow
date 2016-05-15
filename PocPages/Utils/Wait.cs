52ccf8acb05b46c57c3d7350fe3f374c3e3c1c25
geeksree <sreenivasparimi95@live.in>
2016-05-15 23:51:14 +0530

    Framework Structure Added

--- PocPages/Utils/Wait.cs
+++ PocPages/Utils/Wait.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UIAccess;
 using UIAccess.WebControls;
@@ -11,22 +12,46 @@ namespace SpecFlowPoc.PocPages.Utils
     public class Wait
     {
         private WebDriverPlugin thisWebDriver;
-
         private string thisGuiMapPath;
 
-
-        public Wait(WebDriverPlugin webDriver, string completeGraphicalUserInterfaceMapPath)
+        public Wait(WebDriverPlugin webDriver, string completeGuiMapPath)
         {
             thisWebDriver = webDriver;
-            thisGuiMapPath = completeGraphicalUserInterfaceMapPath;
+            thisGuiMapPath = completeGuiMapPath;
         }
 
+        public bool IsPresent<T>(string logicalName, int waitBeforeCheck, int timeout) where T : WebControl
+        {
+            Thread.Sleep(waitBeforeCheck);
 
-        //public T GetControl<T>(string guiMap, string logicalName) where T: WebControl
-        //{
-        //    T ctrl = null;
-        //    ctrl = thisWebDriver.WaitForControl<T>(guiMap,logicalName,)
-        //}
+            T Ctrl = null;
+
+            if (null == thisWebDriver.WaitForControl<T>(thisGuiMapPath, logicalName,
+                                                timeout))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public T GetHtmlControl<T>(string GUIMap, string LogicalName) where T : WebControl
+        {
+            T Ctrl = null;
+
+            Ctrl = thisWebDriver.WaitForControl<T>(GUIMap, LogicalName,
+                                                Config.PageClassSettings.Default.MaxTimeoutValue);
+            if (Ctrl == null)
+            {
+                //throw new GUIException(LogicalName, "Element not found on the Screen");
+            }
+            return Ctrl;
+        }
+
+        public T GetHtmlControl<T>(string logicalName) where T : WebControl
+        {
+            return GetHtmlControl<T>(thisGuiMapPath, logicalName);
+        }
 
     }
 }
---
+++ PocTest/Config/TestSettings.cs
@@ -0,0 +1,191 @@
+﻿using AuScGen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowPoc.PocTest.Config
+{
+    class TestSettings : BaseSetings
+    {
+        private static TestSettings defaultInstance = new TestSettings();
+        private static string settingsFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Config");
+        private static string settingsFile;
+                
+        public static TestSettings Default
+        {
+            get
+            {
+                settingsFile = Path.Combine(settingsFilePath, "TestSettings.xml");
+                return defaultInstance;
+            }
+
+        }
+
+        string browser = "InternetExplorer";
+        public string Browser
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "Browser");
+                if (null != value)
+                {
+                    url = value;
+                }
+                return url;
+                //return ((string)(this["Url"]));
+            }
+            //set
+            //{
+            //    this["Url"] = value;
+            //}
+        }
+
+       
+        string url = "";
+        public string Url
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "Url");
+                if (null != value)
+                {
+                    url = value;
+                }
+                return url;
+                //return ((string)(this["Url"]));
+            }
+            //set
+            //{
+            //    this["Url"] = value;
+            //}
+        }
+
+        string auurl = "";
+        public string AUUrl
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "AUDigitalUrl");
+                if (null != value)
+                {
+                    auurl = value;
+                }
+                return auurl;                
+            }            
+        }
+
+        string aiurl = "";
+        public string AIUrl
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "AIDigitalUrl");
+                if (null != value)
+                {
+                    aiurl = value;
+                }
+                return aiurl;
+            }
+        }
+
+        string suurl = "";
+        public string SUUrl
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "SUDigitalUrl");
+                if (null != value)
+                {
+                    suurl = value;
+                }
+                return suurl;
+            }
+        }
+
+        string BMCurl = "";
+        public string BMCUrl
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "BMCDigitalUrl");
+                if (null != value)
+                {
+                    BMCurl = value;
+                }
+                return BMCurl;
+            }
+        }
+
+        string dBConnection = "";
+        public string DBConnection
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "DBConnection");
+                if (null != value)
+                {
+                    dBConnection = value;
+                }
+                return dBConnection;
+                //return ((string)(this["DBConnection"]));
+            }
+            //set
+            //{
+            //    this["DBConnection"] = value;
+            //}
+        }
+
+        
+        string getByFilterAPI = "";
+        public string GetByFilterAPI
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "GetByFilterAPI");
+                if (null != value)
+                {
+                    getByFilterAPI = value;
+                }
+                return getByFilterAPI;
+                //return ((string)(this["GetByFilterAPI"]));
+            }
+            //set
+            //{
+            //    this["GetByFilterAPI"] = value;
+            //}
+        }
+
+        //[global::System.Configuration.DefaultSettingValueAttribute("False")]
+        bool userViewMode = false;
+        public bool UserViewMode
+        {
+            get
+            {
+                string value = GetValue(settingsFile, "UserViewMode");
+                if (null != value)
+                {
+                    userViewMode = value.ToLower().Contains("true");
+
+                }
+                return userViewMode;
+                //return ((bool)(this["UserViewMode"]));
+            }
+            //set
+            //{
+            //    this["UserViewMode"] = value;
+            //}
+        }
+
+        public string BinaryPath
+        {
+            get
+            {
+                return GetValue(settingsFile, "BinaryPath");
+            }
+        }
+
+    }
+}
